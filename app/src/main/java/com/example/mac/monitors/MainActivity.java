f50b0b7711a7cc1e885216658b278e4fa8b190ee
bunny <chendongming@rsdalstar-rd.com>
2017-11-23 19:05:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mac/monitors/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mac.monitors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: 2017/5/22 该项目主要分析安卓Monitor的使用分析 
+    }
+}
